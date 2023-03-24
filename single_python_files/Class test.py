2cac75bfaf53936d120c0021d9d50408ef511667
Alex-DSP <alex.dsp2003@gamil.com>
2023-03-24 19:06:46 +0000

    Fixed now

--- single_python_files/Class test.py
+++ single_python_files/Class test.py
@@ -5,22 +5,24 @@ class Cats:
         return "cats are great"
     def Rating(self):  
         if self._rating > 50:
-           print("this cat is good")
+           print("this cat is good its rating is ",self._rating)
         elif self._rating == 50:
-           print("this cat is mid")
+           print("this cat is mid its rating is ",self._rating)
         else:
-            print("this cat sucks")
+            print("this cat sucks its rating is ",self._rating)
     def Better(self, ratingnumber):
         self._rating = self._rating + ratingnumber
         print("rating improved by ", ratingnumber)
-    def Worse(self):
-        self._rating = self._rating - 25
-        print("rating reduced")
+    def Worse(self, ratingnumber):
+        self._rating = self._rating - ratingnumber
+        print("rating reduced by ", ratingnumber)
 
 print(Cats.opinion())
 splodge = Cats()
 timtam = Cats()
+mob = Cats()
 splodge.Better(25)
-timtam.Worse()
-splodge.Rating
-timtam.Rating
+mob.Worse(50)
+splodge.Rating()
+timtam.Rating()
+mob.Rating()
