48e6e021eb5207ef14c9e3b0e8e1fb73e1228723
pauldestpaer <pauldestpaer@gmail.com>
2023-03-30 20:19:30 +0100

    json

--- single_python_files/Datareader.py
+++ single_python_files/Datareader.py
@@ -1,12 +1,15 @@
 FD = []
 with  open("single_python_files\\data\\1_1_in.txt") as file: #opens the file but only while its accessed by the chunk in with
     for line in file: #adds the line to the list for all lines in the file
-        FD.append(line)
+        if line.strip().isnumeric():
+            FD.append(int(line.strip()))
 
 FD.sort() #sorts the list
+
+print(sum(FD))
     
-for i in FD: #prints the whole list, since it was sorted it prints it out ascending 
-    print(i)
+#for i in FD: #prints the whole list, since it was sorted it prints it out ascending 
+    #print(i)
 
 
 
