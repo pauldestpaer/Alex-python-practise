9407692ca65d3205b17388d0fb3238baca0ecc71
Alex-DSP <alex.dsp2003@gamil.com>
2023-04-03 16:14:59 +0100

    sorting

--- single_python_files/Datareader.py
+++ single_python_files/Datareader.py
@@ -1,12 +1,20 @@
 FD = []
+total = int (0)
 with  open("single_python_files\\data\\1_1_in.txt") as file: #opens the file but only while its accessed by the chunk in with
     for line in file: #adds the line to the list for all lines in the file
-        FD.append(line)
+        line = line.strip()
+        if line != "":
+             FD.append(line)
+        
 
 FD.sort() #sorts the list
     
-for i in FD: #prints the whole list, since it was sorted it prints it out ascending 
-    print(i)
+#for i in FD: #prints the whole list, since it was sorted it prints it out ascending  
+#    print(i)
+
+
+print (sum(int(i) for i in FD))
+
 
 
 
